526856cedc4119d9f7323ec624dd38f8f087f84f
vivek <vivekrockss111@gmail.com>
2023-12-18 22:13:06 +0530

    ok

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,21 +1,26 @@
 import './contact.css'
 import whatsapp from '../asset/icons8-whatsapp-48.png';
 import location from '../asset/icons8-location-50.png';
+import email from '../asset/gmail_732200.png';
+
 export default function Contact(){
 
     return(
         <>
         <p className="main-head ">Contact US</p>
         <div className='m'>
-        <div className='for'>
+        <a href='https://wa.me/+16072951196' target='_blank' ><div className='for'>
 
-        <img src={whatsapp}  />+16-072951196
+        <img src={whatsapp}  />+1 607-295-1196
+        </div></a>
+        <a href='https://wa.me/+12092004304' target='_blank' > <div className='for'>
+        <img src={whatsapp}/>+1 209-200-4304
+        </div></a>
+        <div className='for'>
+        <img src={email} width={35}/>rishi@protaxation.net
         </div>
         <div className='for'>
-        <img src={whatsapp}/>+12-092004304
-        </div>
-        <div className='for'>
-            <img src={location}/>4795 Commercial Drive,
+            <img src={location}/>4795 Commercial Drive
 New Hartford, NY 13413
          </div>
          </div>
