b861aab1c40c64ab550230f9e03a8198aba2226d
vivek <vivekrockss111@gmail.com>
2025-01-31 18:40:12 +0530

    updated contacts

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,29 +1,29 @@
-import './contact.css'
-import whatsapp from '../asset/icons8-whatsapp-48.png';
-import location from '../asset/icons8-location-50.png';
-import email from '../asset/gmail_732200.png';
+import './contact.css';
 
-export default function Contact(){
-
-    return(
+export default function Contact() {
+    return (
         <>
-        <p className="main-head ">Contact US</p>
-        <div className='m'>
-        <a href='https://wa.me/+16072951196' target='_blank' ><div className='for'>
-
-        <img src={whatsapp}  />+1 607-564-5355
-        </div>
-        <div className='for'>
-        <img src={whatsapp}/>+1 213-799-4574
-        </div>
-        <div className='for'>
-        <img src={email} width={35}/>rishi@protaxations.com / saanvi@protaxations.com
-        </div>
-        <div className='for'>
-            <img src={location}/>4795 Commercial Drive,
-New Hartford, NY 13413
-         </div>
-         </div>
+            <p className="main-head">Contact Us</p>
+            <div className='m'>
+                <a href='https://wa.me/16075645355' target='_blank' rel="noopener noreferrer">
+                    <div className='for'>
+                        <img src="/assets/icons8-whatsapp-48.png" alt="WhatsApp" />
+                        +1 607-564-5355
+                    </div>
+                </a>
+                <div className='for'>
+                    <img src="/assets/icons8-whatsapp-48.png" alt="WhatsApp" />
+                    +1 213-799-4574
+                </div>
+                <div className='for'>
+                    <img src="/assets/gmail_732200.png" width={35} alt="Email" />
+                    rishi@protaxations.com / saanvi@protaxations.com
+                </div>
+                <div className='for'>
+                    <img src="/assets/icons8-location-50.png" alt="Location" />
+                    4795 Commercial Drive, New Hartford, NY 13413
+                </div>
+            </div>
         </>
     );
 }
